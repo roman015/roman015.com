7a3be669307a24bac9dd5ab00ed547121e46ee32
roman015 <xaviervarricatt+roman015@live.in>
2020-05-07 12:56:52 +0800

    Removed https redirection

--- blog/Startup.cs
+++ blog/Startup.cs
@@ -40,7 +40,6 @@ namespace blog.roman015.com
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
