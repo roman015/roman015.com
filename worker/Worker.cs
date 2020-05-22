5cd62a9f64cf31271132fd2dfbeff97e769072a8
Abraham V <abraham.varricatt@googlemail.com>
2020-05-22 18:50:44 -0400

    Hello World request-response from worker

--- worker/Worker.cs
+++ worker/Worker.cs
@@ -22,33 +22,65 @@ namespace dotNetWorker
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            // This method represents the background service we are running
+
+
             _logger.LogInformation("Starting worker service!");
 
             _client = new DiscordSocketClient(new DiscordSocketConfig
             {
-                LogLevel = LogSeverity.Info
+                LogLevel = LogSeverity.Info,
+                MessageCacheSize = 100
             });
-            _client.Log += Log;
+
+            //Hook into log event
+            _client.Log += LogAsync;
+
+            //Hook into the client ready event
+            _client.Ready += ReadyAsync;
+
+            //Hook into the message received event, this is how we handle the hello world example
+            _client.MessageReceived += MessageReceivedAsync;
 
             var token = Environment.GetEnvironmentVariable("WORKER_DISCORD_BOT_TOKEN");
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
 
-            _client.Ready += () => 
-            {
-                _logger.LogInformation("Bot is connected!");
-                return Task.CompletedTask;
-            };
-
             // Block this task until the program is closed.
             await Task.Delay(-1);
         }
 
-        private Task Log(LogMessage message)
+        public override async Task StopAsync(CancellationToken stoppingToken)
+        {
+            // This method is called to gracefully shut down the worker
+            _logger.LogInformation("Gracefully stopping worker service.");
+
+            await Task.CompletedTask;
+        }
+
+        private Task LogAsync(LogMessage message)
         {
             // Write logs to systemd/journalctl directly
             _logger.LogInformation(message.ToString());
             return Task.CompletedTask;
+        }
+
+        private Task ReadyAsync()
+        {
+            _logger.LogInformation($"Connected as -> [] :)");
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(SocketMessage message)
+        {
+            //This ensures we don't loop things by responding to ourselves (as the bot)
+            if (message.Author.Id == _client.CurrentUser.Id)
+                return;
+
+            if (message.Content == ".hello")
+            {
+                await message.Channel.SendMessageAsync("world!");
+            }  
         }
     }
 }
