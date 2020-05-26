11e5834df7ee13255001cff5251e994968e62272
Abraham V <abraham.varricatt@googlemail.com>
2020-05-26 11:05:41 -0400

    More log messages and assingment test.

--- worker/Services/ExampleTaskBoss.cs
+++ worker/Services/ExampleTaskBoss.cs
@@ -4,6 +4,7 @@ using Discord;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 
 namespace dotNetWorker.Services
@@ -12,27 +13,38 @@ namespace dotNetWorker.Services
     {
         private readonly IConfiguration _config;
         private readonly DiscordSocketClient _client;
-        private readonly IMessageChannel _channel;
+        private readonly Microsoft.Extensions.Logging.ILogger _logger;
 
         public ExampleTaskBoss(IServiceProvider services)
         {
             _client = services.GetRequiredService<DiscordSocketClient>();
-            _config = services.GetRequiredService<IConfiguration>();
-            _channel = (_client
-                .GetChannel(ulong.Parse(_config["channel-shell-output"])) as IMessageChannel);
+            _config = services.GetRequiredService<IConfiguration>();            
+            _logger = services.GetRequiredService<ILogger<CommandHandler>>();
         }
         public async void StartTask()
         {
+            _logger.LogInformation("begin StartTask() ");
             await (_client
                 .GetChannel(ulong.Parse(_config["channel-shell-output"])) as IMessageChannel)
                 .SendMessageAsync("hello! from typecasting");
+
+            var temp_socket_channel = _client
+                .GetChannel(ulong.Parse(_config["channel-shell-output"]));
+            
+            if (temp_socket_channel == null) {
+                _logger.LogInformation("received null, returning");
+                return;
+            }
+            _logger.LogInformation("got something, attempting typecast");
+
+            var channel = temp_socket_channel as IMessageChannel;
             // TODO: start a task, for now just write a message into discord
-            await _channel.SendMessageAsync("hello from object!");
+            await channel.SendMessageAsync("hello from object!");
             for (int i = 1; i <= 5; i++) { 
                 await Task.Delay(2000);
-                await _channel.SendMessageAsync($"Loop iteration = {i}");
+                await channel.SendMessageAsync($"Loop iteration = {i}");
             }
-            await _channel.SendMessageAsync("Loop over");
+            await channel.SendMessageAsync("Loop over");
         }
     }
 }
