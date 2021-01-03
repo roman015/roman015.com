99e8cf96af0806d4a76ff770e8bca8064a9c381d
roman015 <xaviervarricatt+roman015@live.in>
2021-01-03 19:58:09 +0800

    Auth0 Authentication Added

--- Program.cs
+++ Program.cs
@@ -18,7 +18,14 @@ namespace HomePage
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { 
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
+            });
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                builder.Configuration.Bind("Auth0", options.ProviderOptions);
+                options.ProviderOptions.ResponseType = "code";
+            });
             builder.Services.AddBlazoredLocalStorage();
 
             await builder.Build().RunAsync();
