3781268cb3fdf1eb47990696d15b8c2193efc069
roman015 <xaviervarricatt+roman015@live.in>
2021-06-20 23:42:46 +0800

    Replaced Wyam Static Generator With Blazor

---
+++ wwwroot/assets/js/app.js
@@ -0,0 +1,27 @@
+﻿function CreateTriangles() {
+    // Header background                        
+    var colors = Please.make_color({
+        colors_returned: 3,
+        saturation: .6
+    });
+    var t = new Trianglify({
+        x_gradient: colors,
+        y_gradient: ["#FFFFFF"]
+    });
+    var header = document.getElementById("intro-header");
+    var pattern = t.generate(header.clientWidth, header.clientHeight);
+    header.setAttribute('style', 'background-image: ' + pattern.dataUrl);
+}
+
+function SetPageTitle(newTitle) {
+    this.window.document.title = newTitle;
+}
+
+
+// TODO: Find out what this is for
+function EnableBackgroundCheck() {
+    BackgroundCheck.init({
+        targets: '.intro-header',
+        images: '.intro-header'
+    });
+}
