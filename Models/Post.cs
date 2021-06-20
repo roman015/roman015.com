2816d7b697adf4efd59d325abebae4b9c3f23625
roman015 <xaviervarricatt+roman015@live.in>
2021-06-20 16:36:45 +0800

    Fix for constructor

--- Models/Post.cs
+++ Models/Post.cs
@@ -19,28 +19,27 @@ namespace Roman015API.Models
 
         public Post(string PostID, string MarkDownFileContent, DateTime? LastModifiedOn = null)
         {
-            var result = new Post();
-
             var data = MarkDownFileContent
                         .Split(Environment.NewLine)
                         .Take(3)
                         .Select(item => item.Substring(item.IndexOf(":") + 1).Trim())
                         .ToList();
 
-            result.ID = PostID;
-            result.Title = data[0];
-            result.PublishedOn = DateTime.ParseExact(data[1], "MM/dd/yyyy", CultureInfo.InvariantCulture);
-            result.LastModifiedOn = LastModifiedOn.HasValue ? LastModifiedOn.Value : result.PublishedOn;
-            result.Tags = data[2]
+            this.ID = PostID;
+            this.Title = data[0];
+            this.PublishedOn = DateTime.ParseExact(data[1], "MM/dd/yyyy", CultureInfo.InvariantCulture);
+            this.LastModifiedOn = LastModifiedOn.HasValue ? LastModifiedOn.Value : this.PublishedOn;
+            this.Tags = data[2]
                     .Substring(1, data[2].Length - 2) // Remove []
                     .Trim()
                     .Split(",")
                     .Select(item => item.Trim().Replace("\"", string.Empty)) // To remove the double quotes from each entry
                     .ToList();
-            result.PostMarkDown = MarkDownFileContent
+            this.PostMarkDown = MarkDownFileContent
                     .Substring(MarkDownFileContent.IndexOf("---") + "---".Length)
                     .Split(Environment.NewLine)
                     .First(); // Only the first line of the markdown
+            
         }
     }
 }
