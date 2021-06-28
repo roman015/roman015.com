3cb07a07bc7f12f6c0a5454a5790356661226b20
roman015 <xaviervarricatt+roman015@live.in>
2021-06-29 01:07:30 +0800

    Added Cache Warmup

---
+++ Services/CacheWarmupService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Roman015API.Services
+{
+    public class CacheWarmupService : IHostedService, IDisposable
+    {
+        private Timer timer;       
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            timer = new Timer(async (state) =>
+            {
+                // Just call Any BlogViewerController API to keep the cache warm
+                var result = await new HttpClient().GetStringAsync(@"http://localhost/BlogViewer/GetAllTags");
+                Console.WriteLine(result);
+            }, 
+            null, 
+            TimeSpan.Zero,
+            TimeSpan.FromHours(3));
+
+            return Task.CompletedTask;
+        }        
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            timer?.Dispose();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Identity.Web;
+using Roman015API.Services;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -49,7 +50,10 @@ namespace Roman015API
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)                
                 .AddMicrosoftIdentityWebApi(Configuration.GetSection("AzureAd"));            
 
-            services.AddControllers();            
+            services.AddControllers();
+
+            services.AddHostedService<CacheWarmupService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
