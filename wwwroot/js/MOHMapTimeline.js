544a2ba1d3155a32e02d130e4d2491fb1758fc31
roman015 <xaviervarricatt+roman015@live.in>
2021-07-02 11:03:10 +0800

    Added Infobox to MOHTimeline Map

--- wwwroot/js/MOHMapTimeline.js
+++ wwwroot/js/MOHMapTimeline.js
@@ -1,22 +1,39 @@
 ﻿var map;
+var infobox;
 
 export function loadMap(bingMapsKey) {
     map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: bingMapsKey,
         center: new Microsoft.Maps.Location(1.290160, 103.852000) // Lat, Lon of "Singapore"
     });
+
+    // Setup Infobox
+    infobox = new Microsoft.Maps.Infobox(map.getCenter(), {
+        visible: false
+    });
+    infobox.setMap(map);
 }
 
 export function pushPin(newPin) {
     console.log(newPin);
-    //Create custom Pushpin
+
+    // Create custom Pushpin
     var pin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(newPin.latitude, newPin.longitude), {
         title: newPin.title,
         color: newPin.color,
         subTitle: newPin.subtitle
     });
 
-    //Add the pushpin to the map
+    pin.metadata = {
+        pdfUrl: newPin.pdfUrl,
+        articleUrl: newPin.articleUrl,
+        location: newPin.locationFormatted
+    }
+
+    // Add Click Event Handler
+    Microsoft.Maps.Events.addHandler(pin, 'click', displayInfobox);
+
+    // Add the pushpin to the map
     map.entities.push(pin);    
 }
 
@@ -41,4 +58,29 @@ export function removeAllPins() {
         }
     }
     console.log(count + ' Pins Removed');
+}
+
+export function displayInfobox(e) {
+    if (e.target.metadata) {
+        infobox.setOptions({
+            maxHeight: 640,
+            maxWidth: 640,
+            location: e.target.getLocation(),
+            title: e.title + (e.subTitle),
+            description: e.target.metadata.location,
+            visible: true,
+            actions: [{
+                label: 'Press Release',
+                eventHandler: function () {
+                    window.open(e.target.metadata.articleUrl, '_blank');
+                }
+            },
+            {
+                label: 'PDF',
+                eventHandler: function () {
+                    window.open(e.target.metadata.pdfUrl, '_blank');
+                }
+            }]
+        });
+    }
 }
